ba0860efab418290dbb172463873c125d94194df
Ivan5646 <Mike000027@yandex.ru>
2018-04-14 05:01:43 +0700

    redux logger

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,12 +1,24 @@
 import 'babel-polyfill'; 
 import React from 'react';
-import ReactDOM from "react-dom"; // all above imported from node modules
+import logger from 'redux-logger'; 
+import thunk from 'redux-thunk'; 
+import {applyMiddleware} from 'redux';
+import ReactDOM from "react-dom"; 
 import {Provider} from 'react-redux';
 import {createStore} from "redux"; // {} means import a var or function from package
 import allReducers from "./reducers";
 import App from './components/app';
 
-const store = createStore(allReducers);
+const middleware = [ thunk ]
+if (process.env.NODE_ENV !== 'production') {
+  // middleware.push(createLogger()) // createLogger throwing error
+}
+
+// const store = createStore(allReducers);
+const store = createStore(
+  allReducers,
+  applyMiddleware(...middleware, logger())
+)
 
 // here <Provider store={store}>  every component has access to store data now  
 ReactDOM.render(
