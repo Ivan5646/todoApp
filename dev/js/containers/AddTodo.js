0da47f793b8236b9511f177e65edcde5324a8286
Ivan5646 <Mike000027@yandex.ru>
2018-04-15 21:49:06 +0700

    got value of input

--- dev/js/containers/AddTodo.js
+++ dev/js/containers/AddTodo.js
@@ -3,13 +3,30 @@ import {connect} from 'react-redux';
 
 class AddTodo extends Component {
 
+    constructor(props) {
+        super(props);
 
-        render() {
+        this.state = {
+            name: "default"
+        };
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.logOut = this.logOut.bind(this);
+    }
+
+    handleNameChange(e) {
+       this.setState({name: e.target.value});
+    }
+
+    addItem() {
+        
+    }
+
+    render() {
         return (
             <form>
                 <div> 
                     <label>Name</label>
-                    <input placeholder="enter the name of the task..." />
+                    <input placeholder="enter the name of the task..." onChange={this.handleNameChange} />
                 </div>
                 <div> 
                     <label>Description</label>
@@ -27,8 +44,13 @@ class AddTodo extends Component {
                     <input placeholder="enter the due date of the task..." />
                 </div>
                 <button>Add Task</button>
+                <div>{this.state.name}</div>
             </form>
         )
+    }
+
+    logOut() {
+        console.log(this.state.name);
     }
 }
 
