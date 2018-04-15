1f518ae442c76f3f3be1254773e6d5722605ecfc
Ivan5646 <Mike000027@yandex.ru>
2018-04-16 01:57:29 +0700

    store.subscribe2

--- dev/js/index.js
+++ dev/js/index.js
@@ -3,22 +3,22 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import {Provider} from 'react-redux';
 import {createStore} from "redux"; 
-import App from './components/App';
-import allReducers from "./reducers";
+// import App from './components/App';
+// import allReducers from "./reducers";
 
-const store = createStore(allReducers);
+const store = createStore(test);
 
 // here <Provider store={store}>  every component has access to store data now  
 ReactDOM.render(
     <Provider store={store}>   
-        <App/>
+        <div></div>
     </Provider>,
     document.getElementById('root')
 );
 
-
+// testing redux
 function test(state=[], action) {
-    if (action.type === "ADD_TRACK") {
+    if (action.type === 'ADD_TRACK') {
         return [
             ...state,
             action.payload
@@ -31,4 +31,5 @@ store.subscribe(() => {
     console.log("subscribe", store.getState());
 })
 
-store.dispatch({type: "ADD_TRACK", payload: "smells like teen spirit"})
+store.dispatch({type: 'ADD_TRACK', payload: 'smells like teen spirit'});
+store.dispatch({type: 'ADD_TRACK', payload: 'Enter Sandman'});
--- dev/js/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from "redux";
-import TodoListReducer from "./reducer-todolist";
-
-const allReducers = combineReducers({ // that will be thrown to the Store
-    todoList: TodoListReducer 
-});
-
-export default allReducers;
