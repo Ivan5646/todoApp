d9c243487a9408dd96648571b308b20f42052f4d
Ivan5646 <Mike000027@yandex.ru>
2018-04-17 16:44:59 +0700

    got the item id

--- dev/js/containers/TodoList.js
+++ dev/js/containers/TodoList.js
@@ -9,25 +9,24 @@ class TodoList extends Component {
         this.deleteItem = this.deleteItem.bind(this);
     }
 
-    deleteItem() {
+    deleteItem(itemId) {
         //get the item.id and delete the item
         //todoList.item.id
-        var itemId = document.getElementsByTagName("li")[0].getAttribute("id");
-        var x = document.getElementById("deleteBtn").parentElement;
-        x = x.getAttribute("id");
-        console.log(x);
+
+        console.log(itemId);
+
     }
 
     createList() {
         return this.props.todoList.map((item) => { 
             return (
-                <li key={item.id} id={item.id}> 
+                <li key={item.id}> 
                     <div>Name: {item.name}</div>
                     <div>Description: {item.description}</div>
                     <div>Priority: {item.priority}</div>
                     <div>Due Date: {item.due_date}</div>
                     <div>Complete Date: {item.complete_date}</div>
-                    <button onClick={this.deleteItem} id="deleteBtn">Delete Task</button>
+                    <button onClick={() => this.deleteItem(item.id)} >Delete Task</button>
                 </li>
             );
         });
