f270ad71c3c87812799f105b0441aa9695874132
Ivan5646 <Mike000027@yandex.ru>
2018-04-19 03:21:39 +0700

    mutable default values

--- dev/js/containers/EditItem.js
+++ dev/js/containers/EditItem.js
@@ -4,20 +4,30 @@ import TodoList from './TodoList';
 
 class EditItem extends Component {
 
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            value: event.currentTarget.value
+         })
+    }
 
     render() {
         return (
             <form>
                 <div> 
                     <label>Name</label>
-                    <input type="text" placeholder="enter the name of the task..." value={this.props.name}  ref={(input) => {this.nameInput = input}} />
+                    <input type="text" placeholder="enter the name of the task..." defaultValue={this.props.name} onChange={ this.handleChange }  ref={(input) => {this.nameInput = input}} />
                 </div>
                 <div> 
                     <label>Description</label>
-                    <input placeholder="enter task description..." value={this.props.description}  ref={(input) => {this.description = input}} />
+                    <input placeholder="enter task description..." defaultValue={this.props.description}  ref={(input) => {this.description = input}} />
                 </div>
                 <div> 
-                    <select name="priority" value={this.props.priority}  ref={(input) => {this.priority = input}}>
+                    <select name="priority" defaultValue={this.props.priority}  ref={(input) => {this.priority = input}}>
                         <option value="normal">normal</option>
                         <option value="high">high</option>
                         <option value="urgent">urgent</option>
@@ -25,11 +35,11 @@ class EditItem extends Component {
                 </div>
                 <div> 
                     <label>Due Date</label>
-                    <input placeholder="enter the due date of the task..." value={this.props.due_date}   ref={(input) => {this.due_date = input}}/>
+                    <input placeholder="enter the due date of the task..." defaultValue={this.props.due_date}   ref={(input) => {this.due_date = input}}/>
                 </div>
                 <div> 
                     <label>Complete Date</label>
-                    <input placeholder="enter the complete date of the task..." value={this.props.complete_date}   ref={(input) => {this.due_date = input}}/>
+                    <input placeholder="enter the complete date of the task..." defaultValue={this.props.complete_date}   ref={(input) => {this.due_date = input}}/>
                 </div>
                 <button type="button" onClick={() => <EditItem/>}>Done</button>
             </form>
