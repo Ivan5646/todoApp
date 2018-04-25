bf7474e7dd9d4aa6f6feeb6162903eecde470c10
Ivan5646 <Mike000027@yandex.ru>
2018-04-25 18:12:15 +0700

    datetime-local default value, removed seconds

--- dev/js/containers/AddTodo.js
+++ dev/js/containers/AddTodo.js
@@ -15,7 +15,8 @@ class AddTodo extends Component {
 
     getDate() {
         var date = new Date();
-        var convertedDate = date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2) + "T" + ("0" + (date.getHours())).slice(-2) + ":" + ("0" + (date.getMinutes())).slice(-2) + ":" + ("0" + (date.getSeconds())).slice(-2);
+        date.setDate(date.getDate() + 1);
+        var convertedDate = date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2) + "T" + ("0" + (date.getHours())).slice(-2) + ":" + ("0" + (date.getMinutes())).slice(-2) + ":" + "00";
         console.log(convertedDate);
         return convertedDate;
     }
