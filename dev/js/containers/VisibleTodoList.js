04cb660e1a793ea60e546e88a9a3efbdbe505598
Ivan5646 <Mike000027@yandex.ru>
2018-04-29 04:58:45 +0700

    filter footer with links2

--- dev/js/containers/VisibleTodoList.js
+++ dev/js/containers/VisibleTodoList.js
@@ -2,20 +2,20 @@ import { connect } from 'react-redux'
 import { toggleTodo } from '../actions'
 import TodoList from './TodoList'
 
-const getVisibleTodos = (todos, filter) => {
+const getVisibleTodos = (todos, filter) => { // filter - state.visibilityFilter
   switch (filter) {
     case 'SHOW_ALL':
       return todos
-    // case 'SHOW_COMPLETED':
-    //   return todos.filter(t => t.completed)
-    // case 'SHOW_ACTIVE':
-    //   return todos.filter(t => !t.completed)
+    case 'SHOW_COMPLETED':
+      return todos.filter(t => t.completed)
+    case 'SHOW_ACTIVE':
+      return todos.filter(t => !t.completed)
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    todos: getVisibleTodos(state.todos, state.visibilityFilter)
+    todos: getVisibleTodos(state.todoList, state.visibilityFilter) // state.visibilityFilter - reducers/visibilityFilter.js
   }
 }
 
@@ -27,9 +27,6 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-const VisibleTodoList = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TodoList)
+const VisibleTodoList = connect(mapStateToProps, mapDispatchToProps)(TodoList)
 
 export default VisibleTodoList
